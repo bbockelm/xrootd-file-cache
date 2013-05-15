f53ac25f2c4816bc0e2b1520f9dc04aa08f96433
Alja Mrak Tadel <amraktadel@ucsd.edu>
2013-05-15 15:00:33 -0700

    Introduce File structure and use it instead of Prefetch in Factory map. Usin Prefetch from this structure gets better controll over corner cases.

---
+++ src/File.cc
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#include <sstream>
+#include <fcntl.h>
+
+#include "File.hh"
+#include "IO.hh"
+#include "Prefetch.hh"
+#include "Factory.hh"
+#include "Cache.hh"
+#include "XrdOuc/XrdOucEnv.hh"
+
+using namespace XrdFileCache;
+
+File::File(XrdSysError &log, XrdOss& outputFS, XrdOucCacheIO & inputFile):
+    m_diskDF(0),
+    m_prefetch(0),
+    m_log(log)
+{
+    m_log.Emsg("construction xcf File ", inputFile.Path());
+
+    // look for file on disk first
+    XrdOucEnv myEnv;
+    std::string fname;
+    Cache::getFilePathFromURL(inputFile.Path(), fname);
+    fname = Factory::GetInstance().GetTempDirectory() + fname;
+    int test = -1;  // test is existance of cinfo file
+    {
+        m_log.Emsg("File, ", " test if file is complete on disk");
+        std::string chkname = fname + ".cinfo";
+        XrdOucEnv myEnv;
+        XrdOssDF* testfd = Factory::GetInstance().GetOss()->newFile(Factory::GetInstance().GetUsername().c_str());
+        test = testfd->Open(chkname.c_str(), O_RDONLY, 0600, myEnv);
+    }
+    if (test >= 0 )
+    {
+            m_log.Emsg("File, ", " open file from disk");
+        m_diskDF = Factory::GetInstance().GetOss()->newFile(Factory::GetInstance().GetUsername().c_str());
+        if ( m_diskDF  && m_diskDF->Open(fname.c_str(), O_RDONLY, 0600, myEnv))
+        {
+            m_log.Emsg("File, ", "read from disk");
+            m_diskDF->Open(fname.c_str(), O_RDONLY, 0600, myEnv);
+        }
+    }
+
+    // create prefetch 
+    if (m_diskDF  == 0 || m_diskDF && m_diskDF->getFD() <= 0)
+    {
+        m_log.Emsg("xcfFile ", "Create Prefetch");
+        m_prefetch = new XrdFileCache::Prefetch(log, outputFS, inputFile);
+    }
+}
+
+File::~File()
+{
+    m_log.Emsg("File, ", "Going to destruct.");
+    if (m_prefetch)
+    {
+        m_prefetch->CloseCleanly();
+    }
+
+    delete m_prefetch;
+
+    // AMT:: how diskDF is closed ???
+
+}
+
+
+int File::Read (XrdOucCacheStats &/*Now*/, char *buff, long long off, int size)
+{
+    return 0;
+    if (m_prefetch)
+    {
+    m_log.Emsg("File, ", "Read from Prefetch.");
+        return  m_prefetch->Read(buff, off, size);
+    }
+    else
+    {
+    m_log.Emsg("File, ", "Read from disk.");
+        return m_diskDF->Read(buff, off, size);
+    }
+}
---
+++ src/File.hh
@@ -0,0 +1,28 @@
+#ifndef __XRDFILECACHE_FILE_HH__
+#define __XRDFILECACHE_FILE_HH__
+
+#include <XrdOss/XrdOss.hh>
+#include <XrdOuc/XrdOucCache.hh>
+#include "XrdSys/XrdSysError.hh"
+
+namespace XrdFileCache {
+class Prefetch;
+
+class File
+{
+public:
+    File(XrdSysError &log, XrdOss& outputFS, XrdOucCacheIO & inputFile);
+    virtual ~File();
+
+    Prefetch* GetPrefetch() { return m_prefetch; }
+    int Read (XrdOucCacheStats &Now, char *Buffer, long long Offs, int Length);
+
+
+private:
+    XrdOssDF* m_diskDF;
+    Prefetch* m_prefetch;
+    XrdSysError m_log;
+};
+
+}
+#endif
