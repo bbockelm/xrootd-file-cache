8eecd20de7c757dd27b226ffe5ef4852911dba43
alja <alja@xrootd-proxy.t2.ucsd.edu>
2013-08-27 11:34:17 -0700

    Align with current version of xfc logs.

--- test/parseRec.pl
+++ test/parseRec.pl
@@ -3,98 +3,84 @@
 use strict;
 use warnings;
 
-#my $path = "test.log";
-my $path =  shift(@ARGV);
-
-open( my $fh, '<', $path)
-    or die "Could not open $path!";
-
-# Detach: NumHits[5] NumHitsPrefetch[5] NumHitsDisk[0], NumMissed [1061],
-# BytesGet[561920343697072] BytesGetPrefetch[140653338155696] BytesGetDisk[140648476470272], BytesPass[140652595763376], BytesWrite[140653588801700]  
-#BytesGet[10082080] BytesGetPrefetch[10082080] BytesGetDisk[0], BytesPass[2137401568], BytesWrite[4201200557] root://18@cmsstor314.fnal.go
 
 
-my $reHits = '/(.*)Detach: NumHits\[(\d+)\] NumHitsPrefetch\[(\d+)\] NumHitsDisk\[(\d+)\] NumMissed\[(\d+)\] (.*)';
-
-my $reBytes = 'BytesGet\[(\d+)\] BytesGetPrefetch\[(\d+)\] BytesGetDisk\[(\d+)\] BytesPass\[(\d+)\] BytesWrite\[(-?\d*)\]';
-
-#my $re = '/(.*)IO: Detach  NumHits\[(.*)\] NumHitsPrefetch\[(.*)\] NumHitsDisk\[(.*)\], NumMissed \[(.*)\],BytesGet\[(.*)\] BytesGetPrefetch\[(.*)\] BytesGetDisk\[(.*)\]';
-# my $re = '/(.*)IO: Detach  NumHits\[(.*)\] NumHitsPrefetch\[(.*)\] NumHitsDisk\[(.*)\], NumMissed \[(.*)\],BytesGet\[(.*)\] BytesGetPrefetch\[(.*)\]';
-#my $re = '/(.*)IO: Detach  NumHits\[(.*)\] NumHitsPrefetch\[(.*)\] NumHitsDisk\[(.*)\], NumMissed \[(.*)\],BytesGet\[(.*)\]';
-#IO: Detach  NumHits[0] NumHitsPrefetch[0] NumHitsDisk[0], NumMissed [0],BytesGet[0] BytesGetPrefetch[0] BytesGetDisk[0], BytesPass[0], BytesWrite[-1] root://93@cmss   
+my $sbPrefCh   = 0;
+my $sbPref   = 0;
+my $sbDisk   = 0;
+my $sbError   = 0;
 
 
-#my $re = '/(.*)IO: Detach  NumHits\[(.*)\] NumHitsPrefetch\[(.*)\] NumHitsDisk\[(.*)\], NumMissed \[(.*)\], BytesGet\[(.*)\] BytesGetPrefetch\[(.*)\] BytesGetDisk\[(.*)\], BytesPass\[(.*)\], BytesWrite\[(.*)\] (.*)';
-my $c = 0;
-
-my $sumGet = 0;
-my $sumGetPrefetch = 0;
-my $sumGetDisk = 0;
-
-my$sumPass = 1;
-my$sumWrite = 0;
-
-while (my $line = <$fh>){ 
-  # print $line;
-  chomp $line;
-
-  if($line =~ /$reHits/ ) 
-{
-  # file name
-  my $path       = $1;
+my $argc = 0;
+foreach my $arg (@ARGV) {
 
 
-  # hits count;
-  my $hits       = $2;
-  my $hitsPrefetch       = $3;
-  my $hitsDisk       = $4;
-  my $missed     = $5;
- # print $line, "\n\n";
- # print "=== ", $2,", ", $3,", ", $4,", ", $5, "==== NHITS ===================\n";
+  my $path = $arg; #shift(@ARGV);
 
-  # bytes
-  my $bytes = $6;
-#  print "gggg BYE ", $bytes, "\n";
-  if($bytes =~ /$reBytes/ ) { 
-  #  print $1, ", ", $2, ", ", $3, ", ", $4, ", ",  $5, "========= BYTES ==========\n";
+  print "-------------------------------$arg \n";
 
-    my $bytesGet   = $1;
-    my $bytesGetPrefetch   = $2;
-    my $bytesGetDisk   = $3;
-    my $bytesPass  = $4;
-    my $bytesWrite = $5;
+  open( my $fh, '<', $path)
+      or die "Could not open $path!";
 
-    my $name;
-    if ($path =~ /(.*)\/(.*)\?/) {
-      $name = $2;
+
+
+  my $reHits = '/(.*)Detach: NumHits\[(\d+)\] NumHitsPrefetch\[(\d+)\] NumHitsDisk\[(\d+)\] NumMissed\[(\d+)\] (.*)';
+
+# bPrefCh[1825207267] bPref[1828703832] bDisk[0] bError[0] /store/
+  my $reBytes = 'bPrefCh\[(\d+)\] bPref\[(\d+)\] bDisk\[(\d+)\] bError\[(\d+)\] (.*)';
+#my $reBytes = 'bPrefCh\[(\d+)\]';
+
+
+
+  while (my $line = <$fh>){ 
+    # print $line;
+    chomp $line;
+
+    if($line =~ /$reHits/ ) {
+      # file name
+      my $path       = $1;
+
+
+      # hits count;
+      my $hits       = $2;
+      my $hitsPrefetch       = $3;
+      my $hitsDisk       = $4;
+      my $missed     = $5;
+      # print $line, "\n\n";
+      # print "=== ", $2,", ", $3,", ", $4,", ", $5, "==== NHITS ===================\n";
     }
-    printf( "hit =  %-5s miss = %-5s | byteGet %-12s = [ %-10s + %-10s],  bytesPass %-12s, bytesWrite %-12s %-12s\n", $hits, $missed, $bytesGet, $bytesGetPrefetch, $bytesGetDisk, $bytesPass, $bytesWrite, $name); 
+    # bytes
 
-    $sumGet += $bytesGet;
-    $sumGetPrefetch += $bytesGetPrefetch;
-    $sumGetDisk += $bytesGetDisk;
+    if($line =~ /$reBytes/ ) { 
+      #  print $1, ", ", $2, ", ", $3, ", ", $4, ", ",  $5, "========= BYTES ==========\n";
 
-    $sumPass += $bytesPass;
-    $sumWrite += $bytesWrite;
+      my $bPrefCh   = $1;
+      my $bPref   = $2;
+      my $bDisk   = $3;
+      my $bError  = $4;
 
+      $sbPrefCh +=  $bPrefCh;
+      $sbPref  +=  $bPref;
+      $sbDisk  +=  $bDisk;
+      $sbError +=  $bError;
 
-  }
+      my $path = $5;
+      my $name = $5;
+      if ($path =~ /(.*)\/(.*)\.root/) {
+        $name = $2;
+        $name .= ".root";
+      }
+      printf( "bPrefCh %-12s  bPref %-12s | bDisk =  %-12s bError = %-12s name = (%12s)\n", $bPrefCh, $bPref, $bDisk, $bError, $name); 
 
+    }
 
+  } # loop file
 
+} # loop files
 
-  if ($hits == 0 && $missed == 0) { 
-#print " \n !!! detach \n";
-    next;
-  }
-  
-}
-}
+my $bHit = $sbPrefCh +  $sbDisk;
 
-printf( "------------\n summaGet/semPass = %d/%d =  %.2f, sumWrite %d \n", $sumGet, $sumPass, $sumGet/$sumPass, $sumWrite); 
-printf("----- pred/disc = %d/%d \n", $sumGetPrefetch, $sumGetDisk);
-
-
-
+printf( "sbPrefCh %-12s  sbPref %-12s | sbDisk =  %-12s sbError = %-12s \n", $sbPrefCh, $sbPref, $sbDisk, $sbError); 
+printf("bHit %s bMiss %s, ratio %.2f \n", $bHit, $sbPref, $bHit/ ($bHit + $sbPref) )
 
 
